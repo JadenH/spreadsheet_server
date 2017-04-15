f074814c2472abd9617a25642f55983605af515e
marchese <marchese@lab1-14.eng.utah.edu>
2017-04-15 10:55:05 -0600

    Commented, lightly modified copied code

--- exampleEchoServer.cpp
+++ exampleEchoServer.cpp
@@ -5,56 +5,84 @@
  */
 
 #include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
+using namespace std;
+
 #define BUFFER_SIZE 1024
 #define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
 
-int main (int argc, char *argv[]) {
-  if (argc < 2) on_error("Usage: %s [port]\n", argv[0]);
+int main () {
 
-  int port = atoi(argv[1]);
+  //Open for lab1 machines
+  int port_number = 2112;
 
+  //File descriptors for server and client for multi-threading.
   int server_fd, client_fd, err;
+  
   struct sockaddr_in server, client;
+  
   char buf[BUFFER_SIZE];
 
+  //Use socket type AF_INET (for IPv4)
+  //Use socket type SOCK_Stream (for TCP)
+  //Third argument is 0 in order to use IP.
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) on_error("Could not create socket\n");
+  
+  //Check for failure
+  if (server_fd < 0) 
+  	cout << "Could not create socket\n";
 
-  server.sin_family = AF_INET;
-  server.sin_port = htons(port);
-  server.sin_addr.s_addr = htonl(INADDR_ANY);
+  server.sin_family = AF_INET; // Server uses IPv4
+  server.sin_port = htons(port_number);  //Convert port number to byte order used by network
+  server.sin_addr.s_addr = htonl(INADDR_ANY); //Accept any IP address
 
   int opt_val = 1;
-  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
+  //Set options. Could be useful. Check these out.
+  setsockopt(	server_fd,  	//applying choices to server
+  		SOL_SOCKET, 	//set socket level options
+  		SO_REUSEADDR, 	//Bind will reuse local addresses
+  		&opt_val, 	//Returns an int indicating the options chosen.
+  		sizeof opt_val);//In order to save the int correctly
 
-  err = bind(server_fd, (struct sockaddr *) &server, sizeof(server));
-  if (err < 0) on_error("Could not bind socket\n");
+  //Binds the address in 'sockaddr' to the file descriptor for server
+  if ( bind(server_fd, (struct sockaddr *) &server, sizeof(server)) < 0)
+  	cout << "Could not bind socket\n";
 
-  err = listen(server_fd, 128);
-  if (err < 0) on_error("Could not listen on socket\n");
+  //Tells server to start listening
+  if ( listen(server_fd, 128) )
+  	cout << "Server failed to listen socket\n";
 
-  printf("Server is listening on %d\n", port);
+  printf("Server is listening on %d\n", port_number);
 
   while (1) {
+  
+    //Size of client struct
     socklen_t client_len = sizeof(client);
+    
+    //Assign file descriptor to accepted client.
     client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
 
-    if (client_fd < 0) on_error("Could not establish new connection\n");
+    if (client_fd < 0) 
+    	cout << "Could not establish new connection\n";
 
     while (1) {
+    
+      //Recieves message, stores it in buffer.
       int read = recv(client_fd, buf, BUFFER_SIZE, 0);
 
       if (!read) break; // done reading
-      if (read < 0) on_error("Client read failed\n");
+      if (read < 0)
+      	cout << "Receive from client failed\n";
 
-      err = send(client_fd, buf, read, 0);
-      if (err < 0) on_error("Client write failed\n");
+	//Send the buffer contents back. 
+      if (send(client_fd, buf, read, 0) < 0) 
+      	cout << "Send to client failed\n";
     }
   }
 
